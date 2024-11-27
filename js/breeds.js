d949cdac027af7adabe36e286ac39cd7f32bee1f
AnnaRychkova1 <134734366+AnnaRychkova1@users.noreply.github.com>
2024-11-27 16:18:16 +0000

    href to imgs

--- js/breeds.js
+++ js/breeds.js
@@ -1,16 +1,16 @@
 // Random picture for .my-container
 new RandomImageGenerator(
   [
-    "../images/main/paw.png",
-    "../images/main/paw1.png",
-    // "../images/main/paw2.png",
-    // "../images/main/paw3.png",
-    // "../images/main/paw4.png",
-    "../images/main/bone.png",
-    // "../images/main/bone1.png",
-    // "../images/main/bone2.png",
-    // "../images/main/bone3.png",
-    "../images/main/dog.png",
+    "images/main/paw.png",
+    "images/main/paw1.png",
+    // "images/main/paw2.png",
+    // "images/main/paw3.png",
+    // "images/main/paw4.png",
+    "images/main/bone.png",
+    // "images/main/bone1.png",
+    // "images/main/bone2.png",
+    // "images/main/bone3.png",
+    "images/main/dog.png",
   ],
   ".my-container"
 );
